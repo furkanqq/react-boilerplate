2677815e23525b08cb1ca94b67ecabc369ee9463
furkanqq <133240374+furkanqq@users.noreply.github.com>
2023-10-30 14:44:00 +0300

    main

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -1,12 +1,9 @@
 import { Outlet } from 'react-router-dom';
-import Navigation from './Navigation';
 
 export default function Layout() {
     return (
         <div className="main">
-            <div className="navbar">
-                <Navigation />
-            </div>
+            <div className="navbar"></div>
             <div className="container">
                 <Outlet />
             </div>
